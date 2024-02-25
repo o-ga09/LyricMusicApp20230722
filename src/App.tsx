db1ad85643bf8905c9cc9440a69bd2cdee4ffea2
taiti09 <taitiabe1997@gmail.com>
2024-02-26 07:34:05 +0900

    textalive-react-apiを使用するように修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,62 @@
-import { Box, Button, Heading } from '@chakra-ui/react'
-import { useState } from 'react';
-import { Player } from "textalive-app-api";
+import {
+  PlayerContext,
+  PlayerOverlay,
+  PlayerSeekbar,
+} from "textalive-react-api";
+import { useEffect, useMemo, useState } from "react";
+import { IPlayer, Player, PlayerListener } from "textalive-app-api";
 function App() {
+  const [player, setPlayer] = useState<IPlayer | null>(null);
+  const [position, setPosition] = useState<number>(0);
 
-  const [ word, setWord ] = useState('');
+  // create objects
+  const [mediaElement, setMediaElement] = useState<HTMLDivElement | null>(null);
+  const div = useMemo(
+    () => <div className="media" ref={setMediaElement} />,
+    []
+  );
 
-  // TextAlive Player を作る
-  const player = new Player({ app: { token: "3Ov5bbNqfE2O349E" } });
-  player.addListener({
-    // 動画オブジェクトの準備が整ったとき（楽曲に関する情報を読み込み終わったとき）に呼ばれる
-    onVideoReady: (v) => {
-      // 定期的に呼ばれる各単語の "animate" 関数をセットする
-      let w = player.video.firstWord;
-      while (w) {
-        setWord(w.text);
-        w = w.next;
-      }
-    },
-  });
+  // initialize a video object with a template
+  useEffect(() => {
+    if (!mediaElement) {
+      return;
+    }
+
+    const player = new Player({
+      mediaElement,
+    });
+    const listener: PlayerListener = {
+      onThrottledTimeUpdate(position) {
+        setPosition(Math.round(position));
+      },
+    };
+    player.addListener(listener);
+    player.createFromSongUrl("http://www.youtube.com/watch?v=KdNHFKTKX2s");
+    setPlayer(player);
+    return () => {
+      player.removeListener(listener);
+    };
+  }, [mediaElement]);
 
   return (
-    <Box
-      h='100vh'
-      flexDirection='column'
-      display='flex'
-      justifyContent='center'
-    >
-      <Heading position='fixed' top='0px' w='100%' h='50px' textAlign='center'>Lyric Music App</Heading>
-
-      <Box
-        w='700px'
-        m='20px auto'
-      >
-        <Box w='500px' h='500px' bg='green.400' m='0px auto' fontSize='100px' fontWeight='bold'>{word}</Box>
-      </Box>
-      <Button display='flex' w='100px' m='0px auto'>音楽を再生</Button>
-    </Box>
-  )
+    <>
+      <PlayerContext.Provider value={player as any}>
+        <div
+          style={{ position: "relative", background: "#bce", color: "#fff" }}
+        >
+          <PlayerOverlay />
+          <p style={{ margin: 0, padding: "7em 1em", textAlign: "center" }}>
+            dummy content (current playback position:{" "}
+            <strong>{position}</strong> [ms])
+          </p>
+        </div>
+        <div style={{ marginTop: "8px" }}>
+          <PlayerSeekbar />
+          {div}
+        </div>
+      </PlayerContext.Provider>
+    </>
+  );
 }
 
-export default App
+export default App;
